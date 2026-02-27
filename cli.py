90e335272571cfffe46077e229cc9e3d17388443
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-02-27 13:44:37 +0330

    add copy

--- cli.py
+++ cli.py
@@ -16,6 +16,7 @@ one of the supported tasks and datasets.
 """
 
 import argparse
+import copy
 import os
 import random
 from typing import Tuple
@@ -288,12 +289,12 @@ def main():
             logger.info(f"selected task is {selected_task}")
             for pattern_id in tasks_patterns[selected_task]:
                 logger.info(f"pattern id is {pattern_id}")
-                evaluate_pet_model_cfg = pet_model_cfg
+                evaluate_pet_model_cfg = copy.deepcopy(pet_model_cfg)
                 evaluate_pet_model_cfg.pattern_id = -pattern_id
                 # evaluate_wrapper = pet.init_model(evaluate_pet_model_cfg)
                 # evaluate_wrapper = evaluate_wrapper.set_model(evaluate_wrapper.model)
-                evaluate_wrapper = wrapper
-                evaluate_wrapper = evaluate_wrapper.set_config(evaluate_pet_model_cfg)
+                evaluate_wrapper = copy.deepcopy(wrapper)
+                evaluate_wrapper.config = evaluate_pet_model_cfg
                 logger.info(f"evaluate wrapper pattern id is {evaluate_wrapper.config.pattern_id}")
                 result = pet.evaluate(evaluate_wrapper, train_data, pet_eval_cfg, priming_data=None)
                 scores[pattern_id] = result['scores']['acc']
@@ -302,7 +303,7 @@ def main():
             pet_model_cfg.pattern_id = best_pattern
             # wrapper = pet.init_model(pet_model_cfg)
             # wrapper = wrapper.set_model(wrapper.model)
-            wrapper = wrapper.set_config(pet_model_cfg)
+            wrapper.config = pet_model_cfg
             pet.train_single_model(wrapper, train_data, pet_train_cfg, pet_eval_cfg,
                                    ipet_train_data=None, unlabeled_data=None)
 
