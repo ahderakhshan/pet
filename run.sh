2fcd239e374f0179de57e30a63d1fee445bbe187
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-08-29 00:13:57 +0430

    change name

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ python cli.py --method our_method \
   --model_type xlm-roberta \
   --model_name_or_path xlm-roberta-large \
   --task_name parsinlu-movie-sentiment \
-  --output_dir ./new_model \
+  --output_dir ./new_model_check \
   --pet_max_seq_length 256
