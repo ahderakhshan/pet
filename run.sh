88a8614add4fd4497318fd23ea773c5303942461
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-08-30 21:28:52 +0430

    continue run code

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ python cli.py --method our_method \
   --train_iterations 300 \
   --data_dir ./ \
   --model_type xlm-roberta \
-  --model_name_or_path xlm-roberta-large \
+  --model_name_or_path "/home/user2/fnlp/pet/pet/new_model2_200" \
   --task_name parsinlu-movie-sentiment \
-  --output_dir ./new_model2 \
+  --output_dir ./new_model2_ \
   --pet_max_seq_length 512
