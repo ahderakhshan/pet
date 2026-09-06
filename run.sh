4c0998a7d12cb94f1dc6080425fc70672b2ca404
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-09-06 19:04:17 +0430

    fix typo

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ python cli.py --method our_method \
   --train_iterations 5 \
   --data_dir ./ \
   --model_type xlm-roberta \
-  --model_name_or_path xlm-rboerta-large \
+  --model_name_or_path xlm-roberta-large \
   --task_name parsinlu-movie-sentiment \
   --output_dir ./new_model400it_ \
   --pet_max_seq_length 512
